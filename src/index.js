2d171cf16ecb5e6915975af89fe5ad3ae4b3eb8f
ASlAtanasov <95175287+ASlAtanasov@users.noreply.github.com>
2022-04-30 00:08:47 +0300

    Adding Firebase Hosting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getDatabase, ref } from "firebase/database";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCzFJpEyukhF5i9lVtKR3vExLeUm-fwVqw",
+//   authDomain: "be-green-163a0.firebaseapp.com",
+//   projectId: "be-green-163a0",
+//   storageBucket: "be-green-163a0.appspot.com",
+//   messagingSenderId: "6015756216",
+//   appId: "1:6015756216:web:6616dbb239c817ea7b914d",
+//   measurementId: "G-2F9N3QTKVB"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const db = getDatabase();
+// const reference = ref(db, 'users/', + userId);
+// setImmediate(reference, {
+//   username: name,
+//   email: email,
+// });
+
+// writeUserData("ivo", "ivo@abv.bg");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,4 +36,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+// reportWebVitals();
