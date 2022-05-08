8a66432315356ad9cfdd8151bb2d7ae4e5f69049
ASlAtanasov <95175287+ASlAtanasov@users.noreply.github.com>
2022-05-08 21:04:31 +0300

    logout page created

---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router';
+import * as authService from '../../services/authService';
+import { useAuthUserContext } from '../../contexts/AuthContext';
+
+const Logout = () => {
+    const navigate = useNavigate();
+    const { logout } = useAuthUserContext();
+
+    useEffect(() => {
+        authService.signOutPage()
+            .then(() => {
+                localStorage.removeItem('user');
+                logout();
+                navigate('/');
+                alert('You signed out successfully');
+                //return <Navigate to='home' />;
+            })
+            .catch((error) => {
+                alert(error.message)
+            });
+    }, []);
+
+    // return null;
+};
+
+export default Logout;
